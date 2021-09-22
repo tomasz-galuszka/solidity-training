70945065149817ad9281f129b3da7191c547aba6
Tomasz Gałuszka <tomasz.g@yggdrasilgaming.com>
2021-09-22 15:14:28 +0200

    naming refactor

---
+++ src/supply-chain/ProductManager.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+
+import "./types.sol";
+
+contract Item {
+
+  uint public priceInWei;
+  uint public paidInWei;
+  uint public index;
+
+  ProductManager parentContract;
+
+  constructor(ProductManager _parentContract, uint _priceInWei, uint _index) {
+    parentContract = _parentContract;
+    priceInWei = _priceInWei;
+    index = _index;
+  }
+
+  // send money between smart contracts  ??
+  receive() external payable {
+    require(priceInWei == msg.value, "Item full payment required");
+    require(paidInWei == 0, "Item is already paid");
+
+    paidInWei += msg.value;
+
+    // low-level function to transfer more gas ??
+    // msg.value - full amount sent to our smart contract
+    // !!!! listen to return value (bool if sucessful, 2 - retun value from function)
+    (bool success, ) = address(parentContract)
+      .call{value: msg.value}(abi.encodeWithSignature("buy(uint256)", index));
+
+    require(success, "Buy from parent contract failed, transaction cancelled");
+  }
+
+  fallback () external {}
+
+}
+
+contract ProductManager {
+
+  struct Product {
+    string id;
+    TypesLibrary.SupplyChainStatus status;
+    Item item;
+  }
+
+  event SupplyChainStep(uint _index, TypesLibrary.SupplyChainStatus _status, address _address);
+  mapping(uint => Product) public products;
+  uint public index;
+
+  function create(string memory _id, uint _priceInWei) public {
+    require(_priceInWei > 0, "Price greater than zero required");
+
+    Item item = new Item(this, _priceInWei, index);
+
+    products[index].id = _id;
+    products[index].status = TypesLibrary.SupplyChainStatus.CREATED;
+    products[index].item = item;
+
+    ++index;
+
+    emit SupplyChainStep(item.index(), products[item.index()].status, address(item));
+  }
+
+  function buy(uint _index) public payable {
+    require(products[_index].item.priceInWei() == msg.value, "Only full payment required");
+    require(products[_index].status == TypesLibrary.SupplyChainStatus.CREATED, "Product is further in chain process");
+
+    products[_index].status = TypesLibrary.SupplyChainStatus.PAID;
+
+    emit SupplyChainStep(_index, products[_index].status, address(products[_index].item));
+  }
+
+  function deliver(uint _index) public {
+    require(products[_index].status == TypesLibrary.SupplyChainStatus.PAID, "Product must be paid first");
+
+    products[_index].status = TypesLibrary.SupplyChainStatus.DELIVERED;
+
+    emit SupplyChainStep(_index, products[_index].status, address(products[_index].item));
+  }
+
+}
--- src/supply-chain/types.sol
+++ src/supply-chain/types.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.1;
 
 library TypesLibrary {
 
-  enum SupplyChainState {
+  enum SupplyChainStatus {
     CREATED,
     PAID,
     DELIVERED
