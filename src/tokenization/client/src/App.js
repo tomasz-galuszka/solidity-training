46b9e0b7e5f3b0e50827c4fde2dd08e6be44ded6
Tomasz Gałuszka <tomasz.g@yggdrasilgaming.com>
2021-10-10 08:04:14 +0200

    client init impl

--- src/tokenization/client/src/App.js
+++ src/tokenization/client/src/App.js
@@ -1,31 +1,41 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+
+import MachaTokenContract from "./contracts/MachaToken.json";
+import MachaTokenSaleContract from "./contracts/MachaTokenSale.json";
+import KYCContract from "./contracts/KYCContract.json";
+
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  state = { loaded: false };
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.web3 = await getWeb3();
+      this.accounts = await this.web3.eth.getAccounts();
+      this.networkId = await this.web3.eth.net.getId();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+      this.instanceMachaTokenContract = new this.web3.eth.Contract(
+        MachaTokenContract.abi,
+        MachaTokenContract.networks[this.networkId] && MachaTokenContract.networks[this.networkId].address
+      );
+      this.instanceMachaTokenSaleContract = new this.web3.eth.Contract(
+        MachaTokenSaleContract.abi,
+        MachaTokenSaleContract.networks[this.networkId] && MachaTokenSaleContract.networks[this.networkId].address
+      );
+      this.instanceKYCContract = new this.web3.eth.Contract(
+        KYCContract.abi,
+        KYCContract.networks[this.networkId] && KYCContract.networks[this.networkId].address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({loaded: true});
+
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,21 +45,8 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
@@ -64,7 +61,6 @@ class App extends Component {
         <p>
           Try changing the value stored on <strong>line 42</strong> of App.js.
         </p>
-        <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
   }
