5fc01b30be285878c3321c5905f697c408635f10
Tomasz Gałuszka <tomasz.g@yggdrasilgaming.com>
2021-10-29 09:23:43 +0200

    fix

--- src/tokenization/client/src/App.js
+++ src/tokenization/client/src/App.js
@@ -9,25 +9,18 @@ import { TokenBuyComponent } from './components/token-buy.component'
 
 class App extends Component {
 
-  state = { 
-    loaded: false
-  }
+  state = { loaded: false}
 
   componentDidMount = async () => {
     try {
       this.web3 = await getWeb3()
       this.userAddress = (await this.web3.eth.getAccounts())[0]
       this.matchaTokenService = await new MatchaTokenService(this.web3).initialize()
-      // this.subscribeTransferEvents()
       this.setState({loaded: true})
     } catch (error) {
       alert(`Failed to load web3, accounts, or contract. Check console for details.`,)
       console.error(error)
     }
-  }
-
-  reRender = () => {
-    console.log(this.state)
   }
 
   render() {
@@ -39,7 +32,6 @@ class App extends Component {
         <h1>Matcha open sale !</h1>
         <KycComponent web3={this.web3} address={this.userAddress} />
         <TokenBuyComponent web3={this.web3} address={this.userAddress} />
-
         <TokenSaleDetailsComponent web3={this.web3} userAddress={this.userAddress} />
         <AccountDetailsComponent web3={this.web3} address={this.userAddress} />
       </div>
